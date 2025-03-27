acda973e770969060d1bc1fdd861c99a629f3e5d
Coffee <gladiatorr128@gmail.com>
2025-03-27 13:26:21 +0530

    Add files via upload

---
+++ hianime.js
@@ -0,0 +1,53 @@
+//Banner slider
+
+$(document).ready(function () {
+  $(".banner-slider").owlCarousel(
+    $(".banner-slider").owlCarousel({
+      loop: true,
+      nav: true,
+      navContainer: ".sliders",
+      pullhDrag: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+      },
+    })
+  );
+});
+
+$(document).ready(function () {
+  $(".trending-anime-container").owlCarousel({
+    responsiveClass: true,
+    loop: false,
+    nav: true,
+    navContainer: ".tr-sliders",
+    autoWidth: pageYOffset,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      300: {
+        items: 2,
+      },
+      600: {
+        items: 4,
+      },
+      1000: {
+        items: 5,
+      },
+      1200: {
+        items: 6,
+      },
+      1400: {
+        items: 7,
+      },
+      1600: {
+        items: 8,
+      },
+    },
+  });
+});
